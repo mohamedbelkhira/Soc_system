9e5a2c18c058880a7e01342d7b2ae639437386dd
mohamedbelkhira <mohamedbelkhira09@gmail.com>
2025-03-13 08:58:05 +0100

    fixing component

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,10 +1,10 @@
-import CyberAttackGlobalMap from "@/components/dashboard/CyberAttackGlobalMap";
+
 const DashboardPage = () => {
 
 
   return (
     <div className="space-y-8">
-    <CyberAttackGlobalMap/>
+    hello
     </div>
   );
 };
